51cca8741194133425f043d8767225c461e8044c
jaidasparmenter <jaida.s.parmenter@gmail.com>
2026-04-25 22:41:49 -0600

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
-class Program
+namespace game
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            Console.Write("Enter grade percentage: ");
+            string value= Console.ReadLine();
+
+
+            int x=int.Parse(value);
+            int A=90;
+            int B=80;
+            int C=70;
+            int D=60;
+
+            if(x>=A)                              //&&=and      ||=or    !=not
+            {
+                Console.WriteLine("You have an A.");
+            }
+            else if (x>=B)
+            {
+                Console.WriteLine("You have a B.");
+            }
+            else if (x>=C)
+            {
+                Console.WriteLine("You have a C.");
+            }
+            else if (x>=D)
+            {
+                Console.WriteLine("You have a D.");
+            }
+            else
+            {
+                Console.WriteLine("You have an F.");
+            }
+        }
     }
 }
