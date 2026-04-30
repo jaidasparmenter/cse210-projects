403d67533698068e7a2cd648f5ca8e3c4eaf5c41
jaidasparmenter <jaida.s.parmenter@gmail.com>
2026-04-30 15:55:12 -0600

    Update Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Variables
+        string resp = "y";
+
+        // Loop 1
+        do
+        {         
+            Console.Write("Magic Number? ");
+            string num = Console.ReadLine();
+            int Answer = int.Parse(num);
+            
+            int x;
+
+            do
+            {
+                Console.Write("Hello! What is the magic number (Choose between 0-100)? ");
+                string value = Console.ReadLine();
+                x = int.Parse(value);
+
+                if (Answer == x)
+                {
+                    Console.Write("Congrats! You guessed the magic number!");
+                }
+                else if (Answer > x)
+                {
+                    Console.WriteLine("Too low! Guess again.");
+                }
+                else
+                {
+                    Console.WriteLine("Too high! Guess again.");
+                }
+
+            } while (x != Answer);
+
+            Console.WriteLine("Play again? (y/n); ");
+            resp = Console.ReadLine();
+
+        } while (resp == "y");
+
     }
 }
